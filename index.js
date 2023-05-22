76468b6718bf676b76cc543c783d951069c9cb69
zihadx <blankzihad@gmail.com>
2023-05-23 00:39:52 +0600

    add toy data post in database

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-console.log(process.env.DB_PASS)
+console.log(process.env.DB_PASS);
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4gdacpf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4gdacpf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +25,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("toyZone").collection("toys");
+
+    app.get("/toys", async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+    });
+
+    app.post('/toys', async (req, res)=>{
+      const newToy = req.body
+      console.log(newToy)
+      const result = await toyCollection.insertOne(newToy)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
