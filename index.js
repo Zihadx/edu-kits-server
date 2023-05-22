0b77c77cd8ab19c1dd3c2dfe04a96380cc0c6d6a
zihadx <blankzihad@gmail.com>
2023-05-23 05:59:13 +0600

    get spesific data using id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -48,6 +48,13 @@ async function run() {
         .toArray();
       res.send(result);
     });
+
+    app.get('/toys/:id', async (req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.findOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
