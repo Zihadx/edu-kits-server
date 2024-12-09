191129fbe38a6dca4a292c84a5a6cddd94670389
zihadx <blankzihad@gmail.com>
2024-12-10 00:24:49 +0600

    solve some problem

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 5000;
 // middleware
 console.log(process.env.DB_PASS);
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4gdacpf.mongodb.net/?retryWrites=true&w=majority`;
 
